bbef7471f990b1852f60bf2f2c1079355b682e22
dufrlrn123 <dufrlrn123@gmail.com>
2024-05-31 16:58:48 +0900

    test

---
+++ ticketboy-flab/src/main/java/com/example/ticketboyflab/TicketboyFlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketboyflab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketboyFlabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TicketboyFlabApplication.class, args);
+  }
+
+}
